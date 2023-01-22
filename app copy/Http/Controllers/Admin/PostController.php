b0c1d0eb8cd71e9ed124abac65708ed2f6f7a03f
FRA2721 <cimino.francesco36@gmail.com>
2023-01-22 18:34:16 +0100

    first commit

---
+++ app copy/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Post;
+use App\Models\Type;
+use App\Models\Technology;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+// use Illuminate\Support\Str;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // return all posts data operation (posts list);
+        $posts = Post::all();
+        return view("admin.posts.index", compact("posts"));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $types = Type::all();
+        $technologies = Technology::all();
+        return view("admin.posts.create", compact("types", "technologies"));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePostRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePostRequest $request)
+    {
+        $data_received = $request->validated();
+        $data_received["slug"] = Post::generateSlug($data_received["title"]);
+        // $post = new Post();
+        // $post->fill($data_received);
+        // $post->save();
+        
+        // check for the cover image key
+        if ($request->hasFile("cover_image")) {
+            $path = Storage::put("post_images", $data_received["cover_image"]);
+            $data_received["cover_image"] = $path;
+        }
+
+        $post = Post::create($data_received); //fillable is important...
+
+        if ($request->has("technologies")) {
+            $post->technologies()->attach($data_received["technologies"]);
+        }
+        
+        return redirect()->route("admin.posts.index", $post->slug);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return view('admin.posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        $types = Type::all();
+        $technologies = Type::all();
+        return view("admin.posts.edit", compact("post", "types", "technologies"));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePostRequest  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePostRequest $request, Post $post)
+    {
+        $data_received = $request->all();
+        $data_received["slug"] = Post::generateSlug($data_received["title"]);
+
+        if ($request->hasFile("cover_image")) {
+
+            if ($post->cover_image) {
+                Storage::delete($post->cover_image);
+            }
+
+            $path = Storage::put("post_images", $data_received["cover_image"]);
+            $data_received["cover_image"] = $path;
+        }
+        
+        $post->update($data_received);
+
+        if ($request->has('technologies')) {
+            // sync
+            $post->technologies()->sync($data_received["technologies"]);
+        } else {
+            // detach
+            $post->technologies()->detach();
+        }
+        return redirect()->route("admin.posts.index", $post->slug);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->technologies()->detach();
+        $post->delete();
+        return redirect()->route("admin.posts.index");
+    }
+}
---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.admin')
+
+@section('content')
+  <div class="container">
+    <div class="row justify-content-center mt-4">
+      <div class="col-8">
+        <h1 class="text-center">Add a new post</h1>
+
+        {{-- errors section --}}
+        @if ($errors->any())
+
+          <div class="alert alert-danger">
+            <ul>
+              @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+              @endforeach
+            </ul>
+          </div>
+        @endif
+
+        {{-- form: store action --}}
+        <form action="{{ route('admin.posts.store') }}" method="POST" enctype="multipart/form-data">
+          @csrf
+
+          {{-- title section --}}
+          <div class="mb-3">
+            <label for="title" class="form-label">Title:</label>
+            <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}">
+          </div>
+
+          {{-- type section --}}
+          <div class="mb-3">
+            <label for="types">Type:</label>
+            <select name="type_id" id="types" class="form-select">
+                <option value="">Chose</option>
+
+                {{-- each type iteration --}}
+                @foreach ($types as $type)
+                    <option value="{{ $type->id }}" @selected(old('type_id') == $type->id)>{{ $type->name }}</option>
+                @endforeach
+            </select>
+          </div>
+
+          {{-- technologies section --}}
+          <div class="mb-3">
+            <h5 class="mb-2">Technologies:</h5>
+
+            {{-- form check (to select the technologies) --}}
+            @foreach ($technologies as $technology)
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="{{ $technology->id }}" id="technologies-{{ $technology->id }}" name="technologies[]" @checked(in_array($technology->id, old('technologies', [])))>
+                <label class="form-check-label"for="technologies-{{ $technology->id }}">{{ $technology->name }}</label>
+              </div>
+            @endforeach
+          </div>
+            
+          {{-- link section --}}
+          <div class="mb-3">
+            <label for="link" class="form-label">Link:</label>
+            <input type="text" class="form-control" id="link" name="link"value="{{ old('link') }}">
+          </div>
+          
+          {{-- cover image section --}}
+          <div class="mb-3">
+            <label for="cover_image" class="form-label">Cover image:</label>
+            <input type="file" class="form-control" id="cover_image" name="cover_image">
+          </div>
+
+          {{-- cover image (preview) --}}
+          <div>
+            <img id="image_preview" src="" alt="">
+          </div>
+
+          {{-- description section --}}
+          <div class="mb-3">
+            <label for="description" class="form-label">Description:</label>
+            <textarea class="form-control" id="description" name="description" rows="3">{{ old('description') }}</textarea>
+          </div>
+          
+          {{-- add section --}}
+          <div class="row justify-content-center mt-4 mb-4">
+            <button type="submit" class="btn btn-success row">Add</button>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
+@endsection
